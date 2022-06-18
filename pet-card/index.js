f04b54ae234d273811b3f7d6e3a16238c57aacb6
Temitope Agbor <67988734+Temitheboy@users.noreply.github.com>
2022-06-18 11:14:15 +0100

    template

--- pet-card/index.js
+++ pet-card/index.js
@@ -1,25 +1,29 @@
-const template = document.createElement("template");
-template.innerHTML = `
-<div class="pet-card">
-        <div class="avatar">
-            <img />
-            <div>
-                <div class="details">
-                    <h2></h2>
-                    <div class="info">
-                        <p>Breed: <slot name="breed" /> </p>
-                        <p>Age: <slot name="age" /> </p>
-                    </div>
-                    <div class="actions">
-                        <button id="greet">Hello!</button>
-                        <button id="toggle">View details</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-</div>
-`;
+// const template = document.createElement("template");
+// template.innerHTML = `
+// <div class="pet-card">
+//         <div class="avatar">
+//             <img />
+//             <div>
+//                 <div class="details">
+//                     <h2></h2>
+//                     <div class="info">
+//                         <p>Breed: <slot name="breed" /> </p>
+//                         <p>Age: <slot name="age" /> </p>
+//                     </div>
+//                     <div class="actions">
+//                         <button id="greet">Hello!</button>
+//                         <button id="toggle">View details</button>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+// </div>
+// `;
 
+const template = document.querySelectorAll("template");
+
+function template(){
+    
 class PetCard extends HTMLElement {
     constructor (){
         super();
@@ -29,3 +33,14 @@ class PetCard extends HTMLElement {
 }
 
 export default PetCard
+}
+
+// class PetCard extends HTMLElement {
+//     constructor (){
+//         super();
+//         this.attachShadow({mode: "open"});
+//         this.shadowRoot.appendChild(template.content.cloneNode(true));
+//     }
+// }
+
+// export default PetCard
