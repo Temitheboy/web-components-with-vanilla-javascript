ad20320a5e67ffa1c165a6ed32a68fcb91958bed
Temitope Agbor <67988734+Temitheboy@users.noreply.github.com>
2022-06-18 16:57:13 +0100

    git

--- pet-card/index.js
+++ pet-card/index.js
@@ -1,29 +1,31 @@
-// const template = document.createElement("template");
-// template.innerHTML = `
-// <div class="pet-card">
-//         <div class="avatar">
-//             <img />
-//             <div>
-//                 <div class="details">
-//                     <h2></h2>
-//                     <div class="info">
-//                         <p>Breed: <slot name="breed" /> </p>
-//                         <p>Age: <slot name="age" /> </p>
-//                     </div>
-//                     <div class="actions">
-//                         <button id="greet">Hello!</button>
-//                         <button id="toggle">View details</button>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-// </div>
-// `;
+const template = document.createElement("template");
+template.innerHTML = `
+<div class="pet-card">
+        <div class="avatar">
+            <img />
+            <div>
+                <div class="details">
+                    <h2></h2>
+                    <div class="info">
+                        <p>Breed: <slot name="breed" /> </p>
+                        <p>Age: <slot name="age" /> </p>
+                    </div>
+                    <div class="actions">
+                        <button id="greet">Hello!</button>
+                        <button id="toggle">View details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+`;
 
 const template = document.querySelectorAll("template");
 
 function template(){
     
+
+}
 class PetCard extends HTMLElement {
     constructor (){
         super();
@@ -31,19 +33,8 @@ class PetCard extends HTMLElement {
         this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
     attributeChangedCallback(name, oldValue, newValue){
-        
+
     }
 }
 
 export default PetCard
-}
-
-// class PetCard extends HTMLElement {
-//     constructor (){
-//         super();
-//         this.attachShadow({mode: "open"});
-//         this.shadowRoot.appendChild(template.content.cloneNode(true));
-//     }
-// }
-
-// export default PetCard
