c0228dbd703debcd3877f3c01b8fa3de705dd4fc
Temitope Agbor <67988734+Temitheboy@users.noreply.github.com>
2022-06-18 10:47:15 +0100

    changes

--- pet-card/index.js
+++ pet-card/index.js
@@ -1,1 +1,32 @@
-class PetCard extends HTMLElement
+const template = document.createElement("template");
+template.innerHTML = `
+<div class="pet-card">
+        <div class="avatar">
+            <img />
+            <div>
+                <div class="details">
+                    <h2></h2>
+                    <div class="info">
+                        <p>Breed: <slot name="breed" /> </p>
+                        <p>Age: <slot name="age" /> </p>
+                    </div>
+                    <div class="actions">
+                        <button id="greet">Hello!</button>
+                        <button id="toggle">View details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+
+`
+
+class PetCard extends HTMLElement {
+    constructor (){
+        super();
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+}
+
+export default PetCard
